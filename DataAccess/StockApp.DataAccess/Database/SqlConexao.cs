5ecef930fb2f6945cdcb39d1e8bf8292aec695d1
Hoerkley <hoerkley@gmail.com>
2023-12-01 21:53:44 -0300

    feat: adicionado string de conexao

---
+++ DataAccess/StockApp.DataAccess/Database/SqlConexao.cs
@@ -0,0 +1,10 @@
+﻿namespace StockPhone.DataBase.SqlServer.Conexao
+{
+    internal static class SqlConexao
+    {
+        internal static string StrConexao()
+        {        
+            return @"Data Source=.\SQLEXPRESS;Initial Catalog=StockPhone;User ID=sa;Password=sql2022; Trusted_Connection = False; TrustServerCertificate = True;";
+        }
+    }
+}
